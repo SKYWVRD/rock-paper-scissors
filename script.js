6704ca263a7c70f12ec81234f0a04a2721948758
Sean Boonzaier <boonzaiersa@gmail.com>
2022-06-30 19:48:39 +0200

    Add gameplay loop logic, move playerChoice and computerChoice into gameplay loop

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const choice_array = ['rock', 'paper', 'scissors'];
-let playerSelection = prompt("Choose Rock, Paper or Scissors").toLowerCase
-let computerSelection = computerPlay();
 
-console.log(playRound(playerSelection, computerSelection))
+gameplayLoop();
 
 function computerPlay(){
     let computerChoice = choice_array[Math.floor(Math.random()*3)];
@@ -13,19 +11,53 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection){
 
     let outcomeString = null;
+    let outcome = null;
 
     if(playerSelection === 'rock' && computerSelection === 'scissors' ||
         playerSelection === 'scissors' && computerSelection === 'paper' ||
         playerSelection === 'paper' && computerSelection === 'rock'){
 
-        outcomeString = `You win ${playerSelection} beats ${computerSelection}`
+        outcomeString = `You win ${playerSelection} beats ${computerSelection}`;
+        outcome = 0;
 
     } else if (playerSelection === computerSelection) {
-        outcomeString = `It is a draw, you both chose ${playerSelection}`
+        outcomeString = `It is a draw, you both chose ${playerSelection}`;
+        outcome = 1;
+        
     } else {
-        outcomeString =`You lose ${computerSelection} beats ${playerSelection}`
+        outcomeString =`You lose ${computerSelection} beats ${playerSelection}`;
+        outcome = 2;
     }
 
-    return outcomeString
+    console.log(outcomeString);
+
+    return outcome;
 };
 
+function gameplayLoop(){
+    let playerWins = 0;
+    let computerWins = 0;
+
+    for( let round = 0; round < 5; round++){
+        let computerSelection = computerPlay();
+        let playerSelection = prompt("Choose Rock, Paper or Scissors").toLowerCase()
+        let outcome = playRound(playerSelection, computerSelection);
+
+        switch (outcome) {
+            case 0:
+                playerWins++;
+                break;
+            case 1:
+                break;
+            case 2:
+                computerWins++;
+                break;
+        }
+    }
+
+    if(playerWins > computerWins){
+        console.log(`Congratulations you are a won ${playerWins} rounds to ${computerWins} round`);
+    } else {
+        console.log(`Sorry you lost ${playerWins} rounds to ${computerWins} rounds`);
+    }
+}
