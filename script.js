699dd4e284db62cc8ec5e9c3c0bc66c35be36e05
Sean Boonzaier <boonzaiersa@gmail.com>
2022-06-30 19:27:30 +0200

    Add gameplay logic in playRound function

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-const choice_array = ['rock', 'paper', 'scissors'];
+let playerSelection = prompt("Choose Rock, Paper or Scissors")
+let computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection))
 
 function computerPlay(){
-    let computerChoice = Math.floor(Math.random()*3);
+    const choice_array = ['rock', 'paper', 'scissors'];
+    let computerChoice = choice_array[Math.floor(Math.random()*3)];
     return computerChoice;
-    
+
 };
 
+function playRound(playerSelection, computerSelection){
+
+    let responseString = null;
+
+    if(playerSelection === 'rock' && computerSelection === 'scissors' ||
+        playerSelection === 'scissors' && computerSelection === 'paper' ||
+        playerSelection === 'paper' && computerSelection === 'rock'){
+
+        responseString = `You win ${playerSelection} beats ${computerSelection}`
+
+    } else if (playerSelection === computerSelection) {
+        responseString = `It is a draw, you both chose ${playerSelection}`
+    } else {
+        responseString =`You lose ${computerSelection} beats ${playerSelection}`
+    }
+
+    return responseString
+}
